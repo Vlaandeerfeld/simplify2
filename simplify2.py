66f963eea1982084f5f2273a162901e37dfa1cea
Michael Young <58713796+Vlaandeerfeld@users.noreply.github.com>
2024-03-02 16:55:51 -0330

    UPDATE logic

--- simplify2.py
+++ simplify2.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import os
 
 # Importing CSV files
 def importFiles():
@@ -44,20 +45,24 @@ def simplifyFiles(files, teams):
 	dfPlayerMasterSimplified = dfPlayerMasterSimplified.drop(columns=['FranchiseId'], axis = 1)
 	
 	dfPlayerSkaterRSSimplified = dfPlayerSkaterRS.drop(columns = ['TeamId', 'FranchiseId'], axis = 1)
-	
+	dfPlayerSkaterRSSimplified = dfPlayerSkaterRSSimplified.add_suffix('_RS')
+	dfPlayerSkaterRSSimplified = dfPlayerSkaterRSSimplified.rename(columns={"PlayerId_RS": "PlayerId"})
+
 	dfPlayerSkaterPOSimplified = dfPlayerSkaterPO[dfPlayerSkaterPO['TeamId'].isin([teams])]
 	dfPlayerSkaterPOSimplified = dfPlayerSkaterPOSimplified.drop(columns = ['TeamId', 'FranchiseId'], axis = 1)
+	dfPlayerSkaterPOSimplified = dfPlayerSkaterPOSimplified.add_suffix('_PO')
+	dfPlayerSkaterPOSimplified = dfPlayerSkaterPOSimplified.rename(columns={"PlayerId_PO": "PlayerId"})
 
 	dfPlayerContractSimplified = dfPlayerContract.drop(columns=['Team'])
+	dfPlayerRatings = dfPlayerRatings.rename(columns={"G": "Goalie"})
 
 	dfExport = dfPlayerMasterSimplified.merge(dfPlayerRatings, on = 'PlayerId')
-	dfExport = dfExport.merge(dfPlayerSkaterRSSimplified, on = 'PlayerId', suffixes = ('oalie', None))	
+	dfExport = dfExport.merge(dfPlayerSkaterRSSimplified, on = 'PlayerId')	
 
 	if dfPlayerSkaterPOSimplified.empty == False:
 		dfExport = dfExport.merge(dfPlayerSkaterPOSimplified, on = 'PlayerId', suffixes = ('_RS', '_PO'))	
 
 	dfExport = dfExport.merge(dfPlayerContractSimplified, on = 'PlayerId')
-	print(dfExport)
 	dfTeamLinesSimplified = dfTeamLines[dfTeamLines['TeamId'].isin([teams])]
 	dfTeamLinesSimplified = dfTeamLinesSimplified.drop(dfTeamLinesSimplified.iloc[:, 19:92], axis = 1)
 	dfTeamLinesSimplified = dfTeamLinesSimplified.drop(columns = ['Extra Attacker 1', 'Extra Attacker 2', 'Unnamed: 96'], axis = 1)	
@@ -65,8 +70,6 @@ def simplifyFiles(files, teams):
 	list1 = dfTeamLinesSimplified.values.tolist()
 	list2 = dfPlayerMasterSimplified.values.tolist()
 
-	print(list1)
-	print(list2)
 	for x in range(len(list1)):
 		for z in range(1, len(list1[x])):
 			for a in range(len(list2)):
@@ -77,41 +80,46 @@ def simplifyFiles(files, teams):
 	dfTeamLinesSimplified1 = pd.DataFrame(list1)
 	dfTeamLinesSimplified1 = dfTeamLinesSimplified1.rename(columns={0: "TeamId", 1: "LW1", 2: "C1", 3: "RW1", 4: "LD1", 5: "RD1", 6: "LW2", 7: "C2", 8: "RW2", 9: "LD2", 10: "RD2", 11: "LW3", 12: "C3", 13: "RW3", 14: "LD3", 15: "RD3", 16: "LW4", 17: "C4", 18: "RW4", 19: "G1", 20: "G2"})
 #	dfTeamLinesSimplified = dfTeamLinesSimplified.drop(21, axis = 1)
-	print(dfTeamLinesSimplified1)
-	dfTeamDataSimplified = dfTeamData.drop(dfTeamData.iloc[:, 5:], axis = 1)
+	dfTeamDataSimplified = dfTeamData.drop(dfTeamData.iloc[:, 5:13], axis = 1)
+	dfTeamDataSimplified = dfTeamDataSimplified.drop(dfTeamDataSimplified.iloc[:, 7:], axis = 1)
 	dfTeamDataSimplified = dfTeamDataSimplified.drop(columns=['LeagueId'])
 	
+	dfTeamStats = dfTeamStats.add_suffix('_RS')
+	dfTeamStats = dfTeamStats.rename(columns={'TeamId_RS': 'TeamId'})
 	dfTeamStatsPOSimplified = dfTeamStatsPO[dfTeamStatsPO['TeamId'].isin([teams])]
-	
+	dfTeamStatsPOSimplified = dfTeamStatsPOSimplified.add_suffix('_PO')
+	dfTeamStatsPOSimplified = dfTeamStatsPOSimplified.rename(columns={'TeamId_PO': 'TeamId'})
+
 	dfTeamRecordsSimplified = dfTeamRecords.drop(columns=['League Id', 'Conf Id', 'Div Id', 'Goals For', 'Goals Against'])
 	dfTeamRecordsSimplified = dfTeamRecordsSimplified.rename(columns={'Team Id': 'TeamId'})	
 
 	dfExport2 = dfTeamLinesSimplified1.merge(dfTeamDataSimplified, on = 'TeamId')
-	
+
 	dfExport2 = dfExport2.merge(dfTeamRecordsSimplified, on = 'TeamId')
 	dfExport2 = dfExport2.merge(dfTeamStats, on = 'TeamId')
 
 	if dfTeamStatsPOSimplified.empty == False:
-		dfExport2 = dfExport2.merge(dfTeamStatsPOSimplified, on = 'TeamId', suffixes = ('Team_RS', 'Team_PO'))
+	    dfExport2 = dfExport2.merge(dfTeamStatsPOSimplified, on = 'TeamId', suffixes = ('Team_RS', 'Team_PO'))
 
-	dfTemp2 = pd.DataFrame() 
+	dfTemp2 = pd.DataFrame()
 	dfTemp = pd.DataFrame()
 	
 	for x in range(len(dfExport.index)):	
 		dfTemp = pd.DataFrame(dfExport.iloc[[x]], columns = dfExport.columns)
-		if x != 0:
-			dfTemp = dfTemp.add_suffix(str(x), axis = 1)	
-			dfTemp = dfTemp.drop(columns = "TeamId" + str(x))
+		dfTemp = dfTemp.add_suffix(str(x), axis = 1)	
+#		dfTemp = dfTemp.drop(columns = "TeamId" + str(x))
+		if x == 0:
+			dfTemp = dfTemp.rename(columns={'TeamId0': 'TeamId'})
 		dfTemp = dfTemp.reset_index()
 		dfTemp2 = pd.concat([dfTemp2, dfTemp], axis = 1)
 		dfTemp2 = dfTemp2.drop(columns = 'index')		
 		dfTemp.columns = dfTemp.columns.str.removesuffix(str(x))
+
+	print(dfTemp2)
 		
 	dfExport2 = pd.merge(dfExport2, dfTemp2, on ='TeamId', suffixes = ('_line', None))
-	for x in dfExport2.iloc[:, 0:176]:
-		print(x)
 
-	dfExport2.to_csv('simplifiedCSV/team_master_simplified' + dfExport2['Name'][0] + '.csv', index = False)
+	dfExport2.to_csv('simplifiedCSV/league_master_simplified.csv', index = False, mode = 'a', header=not os.path.exists('simplifiedCSV/league_master_simplified.csv'))
 	
 def main():
 	files = importFiles()
